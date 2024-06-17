ec37478a2286b84fccc0ec47c9dae3765ae2473b
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-17 11:09:36 +0100

    Add additional stocks and update stock list for Australian stock data

--- sim.py
+++ sim.py
@@ -23,4 +23,6 @@ def get_data(stocks, start_date, end_date):
     return mean_returns, cov_matrix
 
 
-stock_list = ['AAPL', 'AMZN', 'MSFT', 'GOOGL', 'FB']
+stock_list = ['AAPL', 'AMZN', 'MSFT', 'GOOGL', 'FB', 'TSLA', 'NVDA', 'AMD', 'INTC', 'CSCO']
+# add the extension '.AX' to the stock_list to get the Australian stock data
+stock_list = [stock + '.AX' for stock in stock_list]
