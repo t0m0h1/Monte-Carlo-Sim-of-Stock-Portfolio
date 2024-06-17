a6609fc65e27e42b53ae416f3c3b92c89eb1c635
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-17 11:40:10 +0100

    Update data source and add weight normalization

--- sim.py
+++ sim.py
@@ -4,13 +4,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt 
-from pandas_datareader import data as pdr
+import yfinance as yf
 
 
 # import the data
 def get_data(stocks, start_date, end_date):
     # Get historical stock data from Yahoo Finance and select the close price
-    data = pdr.get_data_yahoo(stocks, start=start_date, end=end_date)
+    data = yf.download(stocks, start=start_date, end=end_date)
     data = data['Close']
 
     # Calculate the daily returns
@@ -23,9 +23,8 @@ def get_data(stocks, start_date, end_date):
     return mean_returns, cov_matrix
 
 
-stock_list = ['AAPL', 'AMZN', 'MSFT', 'GOOGL', 'FB', 'TSLA', 'NVDA', 'AMD', 'INTC', 'CSCO']
-# add the extension '.AX' to the stock_list to get the Australian stock data
-stock_list = [stock + '.AX' for stock in stock_list]
+stock_list = ['AAPL', 'AMZN', 'MSFT', 'GOOGL', 'TSLA', 'NVDA', 'AMD', 'INTC', 'CSCO']
+# To add the stocks you want to simulate per region, add an extension for example '.AX' for Australian stocks
 
 end_date = dt.datetime.now()
 start_date = end_date - dt.timedelta(days=365)
@@ -33,3 +32,7 @@ start_date = end_date - dt.timedelta(days=365)
 
 mean_returns, cov_matrix = get_data(stock_list, start_date, end_date)
 print(mean_returns)
+
+
+weights = np.random.random(len(mean_returns))
+weights = weights / np.sum(weights) # normalise the weights
